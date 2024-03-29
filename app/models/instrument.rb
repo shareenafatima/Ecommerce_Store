805516383e55f703e45a161c66124b1cb0d8d275
shareena fatima <shareenafatima40@gmail.com>
2024-03-30 03:30:43 +0500

    Stablished relationship

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,2 +1,8 @@
 class Instrument < ApplicationRecord
-end
+  mount_uploader :image, ImageUploader
+  serialize :image, JSON
+  belong_to :user, optional: true
+
+  validates :title, :brand, :price, :model, presence :true
+  validates :description, length: {minimum:1000, too_long: "%{count} character is the maximum allowed."}
+  validates :title, length: {minimum:140, too_long: "%{count} character is the maximum allowed."}
