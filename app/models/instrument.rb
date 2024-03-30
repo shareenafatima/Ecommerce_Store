2dfeddde3c3712466424f9958884df47240cdde1
shareena fatima <shareenafatima40@gmail.com>
2024-03-30 15:53:21 +0500

    add instruments

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,8 +1,18 @@
 class Instrument < ApplicationRecord
   mount_uploader :image, ImageUploader
   serialize :image, JSON
-  belong_to :user, optional: true
+  belongs_to :user, optional: true
 
-  validates :title, :brand, :price, :model, presence :true
+
+  validates :title, :brand, :price, :model, presence: true
   validates :description, length: {minimum:1000, too_long: "%{count} character is the maximum allowed."}
   validates :title, length: {minimum:140, too_long: "%{count} character is the maximum allowed."}
+  validates :price, numericality: {only_integer: true}, length: {maximum:7}
+
+
+  BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS Ibanez Charvel Washburn }
+  FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
+  CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
+
+end
